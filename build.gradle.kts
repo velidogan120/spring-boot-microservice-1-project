251d0ceb5577559dd27d8c4352a51d7f993c4337
VELİ DOĞAN <61507455+velidogan120@users.noreply.github.com>
2025-09-17 04:39:12 +0300

    Disable bootJar and enable jar tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,14 @@ repositories {
     mavenCentral()
 }
 
+bootJar {
+    enabled = false
+}
+
+jar {
+    enabled = true
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
